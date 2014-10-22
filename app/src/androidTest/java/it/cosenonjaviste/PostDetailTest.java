4f0a42137ca23e8fdf3f8f2da334ae0d7751ac84
Fabio Collini <fabio.collini@gmail.com>
2014-10-22 23:46:25 +0200

    Post detail test

---
+++ app/src/androidTest/java/it/cosenonjaviste/PostDetailTest.java
@@ -0,0 +1,22 @@
+package it.cosenonjaviste;
+
+import android.content.Intent;
+
+import it.cosenonjaviste.base.BaseFragmentTest;
+import it.cosenonjaviste.model.Post;
+import it.cosenonjaviste.mvp.PostDetailPresenter;
+
+public class PostDetailTest extends BaseFragmentTest {
+    public PostDetailTest() {
+        super(PostDetailFragment.class, false);
+    }
+
+    @Override protected Intent createActivityIntent() {
+        Post post = new Post(123, "Title", "http://www.cosenonjaviste.it/consumare-un-webservice-restful-con-jersey-e-primefaces/");
+        return createIntent(super.createActivityIntent(), PostDetailPresenter::populateArgs, post);
+    }
+
+    public void testDetailFragment() throws InterruptedException {
+        Thread.sleep(20000);
+    }
+}
