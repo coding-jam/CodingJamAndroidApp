e3daca33fedd167eb1c41bccf9f7a59ee7fffd3e
Fabio Collini <fabio.collini@gmail.com>
2014-11-22 18:35:49 +0100

    Deleted unused methods

--- mvp/src/main/java/it/cosenonjaviste/mvp/page/PageModel.java
+++ mvp/src/main/java/it/cosenonjaviste/mvp/page/PageModel.java
@@ -1,7 +1,5 @@
 package it.cosenonjaviste.mvp.page;
 
-import it.cosenonjaviste.model.Post;
-
 public class PageModel {
 
     String url;
@@ -15,13 +13,5 @@ public class PageModel {
 
     public String getUrl() {
         return url;
-    }
-
-    public void setPost(Post post) {
-        this.url = post.getUrl();
-    }
-
-    public void setUrl(String url) {
-        this.url = url;
     }
 }
