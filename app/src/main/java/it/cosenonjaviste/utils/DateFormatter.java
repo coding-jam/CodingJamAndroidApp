e1197f71609e40af79da532898cec91d80e59b0e
Fabio Collini <fabio.collini@gmail.com>
2015-02-08 23:02:11 +0100

    Private constructor to avoid instantations

--- app/src/main/java/it/cosenonjaviste/utils/DateFormatter.java
+++ app/src/main/java/it/cosenonjaviste/utils/DateFormatter.java
@@ -5,6 +5,10 @@ import android.text.format.DateUtils;
 import java.util.Date;
 
 public class DateFormatter {
+
+    private DateFormatter() {
+    }
+
     public static CharSequence formatDate(Date date) {
         return DateUtils.getRelativeTimeSpanString(date.getTime(), System.currentTimeMillis(), DateUtils.DAY_IN_MILLIS, DateUtils.FORMAT_SHOW_YEAR);
     }
