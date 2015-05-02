5a2fa138f1a2ec18e17a7c57342b662774096bda
Fabio Collini <fabio.collini@gmail.com>
2015-05-02 12:31:16 +0200

    Moved TweetListPresenter in core module

--- core/src/test/java/it/cosenonjaviste/mvp/twitter/TweetListPresenterTest.java
+++ core/src/test/java/it/cosenonjaviste/mvp/twitter/TweetListPresenterTest.java
@@ -1,9 +1,12 @@
 package it.cosenonjaviste.mvp.twitter;
 
+import org.assertj.core.api.Assertions;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
+import org.mockito.Matchers;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.runners.MockitoJUnitRunner;
 
 import it.cosenonjaviste.TestData;
@@ -13,10 +16,6 @@ import it.cosenonjaviste.twitter.TweetListModel;
 import it.cosenonjaviste.twitter.TweetListPresenter;
 import it.cosenonjaviste.twitter.TweetListView;
 import rx.Observable;
-
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.Matchers.eq;
-import static org.mockito.Mockito.when;
 
 @RunWith(MockitoJUnitRunner.class)
 public class TweetListPresenterTest {
@@ -30,34 +29,34 @@ public class TweetListPresenterTest {
     @InjectMocks TweetListPresenter presenter;
 
     @Test public void testLoadTweets() {
-        when(twitterService.loadTweets(eq(1)))
+        Mockito.when(twitterService.loadTweets(Matchers.eq(1)))
                 .thenReturn(TestData.tweets());
 
         TweetListModel model = new TweetListModel();
         testLifeCycle.initAndResume(model, presenter, view);
-        assertThat(model.size()).isEqualTo(10);
+        Assertions.assertThat(model.size()).isEqualTo(10);
     }
 
     @Test public void testRetryAfterError() {
-        when(twitterService.loadTweets(eq(1)))
+        Mockito.when(twitterService.loadTweets(Matchers.eq(1)))
                 .thenReturn(Observable.error(new RuntimeException()));
 
         TweetListModel model = new TweetListModel();
         testLifeCycle.initAndResume(model, presenter, view);
 
-        assertThat(model.isError()).isTrue();
+        Assertions.assertThat(model.isError()).isTrue();
 
-        when(twitterService.loadTweets(eq(1)))
+        Mockito.when(twitterService.loadTweets(Matchers.eq(1)))
                 .thenReturn(TestData.tweets());
 
         presenter.reloadData();
 
-        assertThat(model.isError()).isFalse();
-        assertThat(model.size()).isEqualTo(10);
+        Assertions.assertThat(model.isError()).isFalse();
+        Assertions.assertThat(model.size()).isEqualTo(10);
     }
 
     @Test public void testLoadMoreTweets() {
-        when(twitterService.loadTweets(eq(1)))
+        Mockito.when(twitterService.loadTweets(Matchers.eq(1)))
                 .thenReturn(TestData.tweets());
 
         TweetListModel tweetListModel = new TweetListModel();
@@ -65,6 +64,6 @@ public class TweetListPresenterTest {
 
         presenter.loadNextPage();
 
-        assertThat(tweetListModel.size()).isEqualTo(20);
+        Assertions.assertThat(tweetListModel.size()).isEqualTo(20);
     }
 }
