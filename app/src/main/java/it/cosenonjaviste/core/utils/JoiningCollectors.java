a009c149983d124f175013236ab0f5241cb38e82
Fabio Collini <fabio.collini@gmail.com>
2015-08-15 17:40:27 +0200

    Deleted StringJoiner class

--- app/src/main/java/it/cosenonjaviste/core/utils/JoiningCollectors.java
+++ app/src/main/java/it/cosenonjaviste/core/utils/JoiningCollectors.java
@@ -7,72 +7,28 @@ import com.annimon.stream.function.Supplier;
 
 public class JoiningCollectors {
 
-    /**
-     * Returns a {@code Collector} that concatenates the input elements,
-     * separated by the specified delimiter, in encounter order.
-     *
-     * @param delimiter the delimiter to be used between each element
-     * @return A {@code Collector} which concatenates CharSequence elements,
-     * separated by the specified delimiter, in encounter order
-     */
-    public static Collector<CharSequence, ?, String> joining(CharSequence delimiter) {
-        return joining(delimiter, "", "");
-    }
+    public static Collector<CharSequence, ?, String> joining(final CharSequence delimiter) {
+        return new Collector<CharSequence, StringBuilder, String>() {
 
-    /**
-     * Returns a {@code Collector} that concatenates the input elements,
-     * separated by the specified delimiter, with the specified prefix and
-     * suffix, in encounter order.
-     *
-     * @param delimiter the delimiter to be used between each element
-     * @param prefix    the sequence of characters to be used at the beginning
-     *                  of the joined result
-     * @param suffix    the sequence of characters to be used at the end
-     *                  of the joined result
-     * @return A {@code Collector} which concatenates CharSequence elements,
-     * separated by the specified delimiter, in encounter order
-     */
-    public static Collector<CharSequence, ?, String> joining(CharSequence delimiter,
-                                                             CharSequence prefix,
-                                                             CharSequence suffix) {
-        return new CollectorImpl<>(
-                () -> new StringJoiner(delimiter, prefix, suffix),
-                StringJoiner::add,
-                StringJoiner::toString);
-    }
+            @Override
+            public Supplier<StringBuilder> supplier() {
+                return StringBuilder::new;
+            }
 
-    /**
-     * Simple implementation class for {@code Collector}.
-     *
-     * @param <T> the type of elements to be collected
-     * @param <R> the type of the result
-     */
-    static class CollectorImpl<T, A, R> implements Collector<T, A, R> {
-        private final Supplier<A> supplier;
-        private final BiConsumer<A, T> accumulator;
-        private final Function<A, R> finisher;
+            @Override
+            public BiConsumer<StringBuilder, CharSequence> accumulator() {
+                return (t, u) -> {
+                    if (t.length() > 0) {
+                        t.append(delimiter);
+                    }
+                    t.append(u);
+                };
+            }
 
-        CollectorImpl(Supplier<A> supplier,
-                      BiConsumer<A, T> accumulator,
-                      Function<A, R> finisher) {
-            this.supplier = supplier;
-            this.accumulator = accumulator;
-            this.finisher = finisher;
-        }
-
-        @Override
-        public BiConsumer<A, T> accumulator() {
-            return accumulator;
-        }
-
-        @Override
-        public Supplier<A> supplier() {
-            return supplier;
-        }
-
-        @Override
-        public Function<A, R> finisher() {
-            return finisher;
-        }
+            @Override
+            public Function<StringBuilder, String> finisher() {
+                return StringBuilder::toString;
+            }
+        };
     }
 }
