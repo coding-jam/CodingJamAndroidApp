30159efea2c3ec0a81015f7f9a05792ff42534b8
Fabio Collini <fabio.collini@gmail.com>
2015-08-17 12:20:00 +0200

    Code refactoring

--- androidMvp/src/main/java/it/cosenonjaviste/lib/mvp/MvpFragment.java
+++ androidMvp/src/main/java/it/cosenonjaviste/lib/mvp/MvpFragment.java
@@ -8,26 +8,17 @@ public abstract class MvpFragment<P extends MvpPresenter<?, ?>> extends Fragment
 
     protected P presenter;
 
+    private ListenersRetainedFragment retainedFragment;
+
     protected void init() {
-        P listener = (P) getListener();
-        if (listener == null) {
-            listener = createPresenter();
-            setListener(listener);
+        presenter = (P) retainedFragment.getListener();
+        if (presenter == null) {
+            presenter = createPresenter();
+            retainedFragment.setListener(presenter);
         }
-        presenter = listener;
     }
 
     protected abstract P createPresenter();
-
-    private ListenersRetainedFragment retainedFragment;
-
-    public MvpPresenter<?, ?> getListener() {
-        return retainedFragment.getListener();
-    }
-
-    public void setListener(MvpPresenter<?, ?> listener) {
-        retainedFragment.setListener(listener);
-    }
 
     @Override public void onCreate(Bundle state) {
         super.onCreate(state);
@@ -46,46 +37,32 @@ public abstract class MvpFragment<P extends MvpPresenter<?, ?>> extends Fragment
                 return value;
             }
         };
-        MvpPresenter<?, ?> listener = retainedFragment.getListener();
-        if (listener != null) {
-            listener.loadState(loader);
-        }
+        retainedFragment.getListener().loadState(loader);
     }
 
     @Override public void onResume() {
         super.onResume();
-        MvpPresenter listener = retainedFragment.getListener();
-        if (listener != null) {
-            listener.resume(this);
-        }
+        ((MvpPresenter) retainedFragment.getListener()).resume(this);
     }
 
     @Override public void onPause() {
         super.onPause();
-        MvpPresenter<?, ?> listener = retainedFragment.getListener();
-        if (listener != null) {
-            listener.pause();
-        }
+        retainedFragment.getListener().pause();
     }
 
     @Override public void onDestroy() {
         super.onDestroy();
-        MvpPresenter<?, ?> listener = retainedFragment.getListener();
-        if (listener != null) {
-            listener.detachView();
-        }
+        retainedFragment.getListener().detachView();
     }
 
     @Override public void onSaveInstanceState(Bundle outState) {
         super.onSaveInstanceState(outState);
         MvpPresenter<?, ?> listener = retainedFragment.getListener();
-        if (listener != null) {
-            ObjectSaver saver = new ObjectSaver() {
-                @Override public void save(String key, Object value) {
-                    outState.putParcelable(key, (Parcelable) value);
-                }
-            };
-            listener.saveState(saver);
-        }
+        ObjectSaver saver = new ObjectSaver() {
+            @Override public void save(String key, Object value) {
+                outState.putParcelable(key, (Parcelable) value);
+            }
+        };
+        listener.saveState(saver);
     }
 }
