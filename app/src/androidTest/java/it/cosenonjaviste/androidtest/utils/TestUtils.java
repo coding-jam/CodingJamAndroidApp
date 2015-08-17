3932bad1c7358174fd98a9df21200a1f277e743a
Fabio Collini <fabio.collini@gmail.com>
2015-08-17 22:29:03 +0200

    Commented sleep

--- app/src/androidTest/java/it/cosenonjaviste/androidtest/utils/TestUtils.java
+++ app/src/androidTest/java/it/cosenonjaviste/androidtest/utils/TestUtils.java
@@ -5,15 +5,13 @@ import rx.functions.Action1;
 public class TestUtils {
 
     public static <T> Action1<T> sleepAction() {
-        return o -> {
-            sleep(1);
-        };
+        return o -> sleep(1);
     }
 
     public static void sleep(int seconds) {
-        try {
-            Thread.sleep(seconds * 1000);
-        } catch (InterruptedException ignored) {
-        }
+//        try {
+//            Thread.sleep(seconds * 1000);
+//        } catch (InterruptedException ignored) {
+//        }
     }
 }
