d0956f657b2ba642687c61aa0af148d70b844be1
Fabio Collini <fabio.collini@gmail.com>
2015-08-30 23:15:12 +0200

    Removed static field

--- app/src/main/java/it/cosenonjaviste/ui/utils/AndroidSchedulerManager.java
+++ app/src/main/java/it/cosenonjaviste/ui/utils/AndroidSchedulerManager.java
@@ -2,14 +2,12 @@ package it.cosenonjaviste.ui.utils;
 
 import it.cosenonjaviste.lib.rx.SchedulerManager;
 import rx.Observable;
-import rx.Scheduler;
 import rx.android.schedulers.AndroidSchedulers;
 import rx.schedulers.Schedulers;
 
 public class AndroidSchedulerManager extends SchedulerManager {
-    public static Scheduler io = Schedulers.io();
 
     public <T> Observable<T> bindObservable(Observable<T> observable) {
-        return observable.subscribeOn(io).observeOn(AndroidSchedulers.mainThread());
+        return observable.subscribeOn(Schedulers.io()).observeOn(AndroidSchedulers.mainThread());
     }
 }
