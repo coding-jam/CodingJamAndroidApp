e219ef78f95b8069be9823205b6186f33ac2de37
Fabio Collini <fabio.collini@gmail.com>
2015-09-27 15:40:39 +0200

    Fix weak reference

--- app/src/main/java/it/cosenonjaviste/ui/utils/BindableAdapter.java
+++ app/src/main/java/it/cosenonjaviste/ui/utils/BindableAdapter.java
@@ -9,6 +9,7 @@ import rx.functions.Func1;
 
 public class BindableAdapter<T> extends RecyclerView.Adapter<BindableViewHolder<?, T>> {
 
+    private final ObservableList.OnListChangedCallback<ObservableList<T>> onListChangedCallback;
     private ObservableArrayList<T> items;
 
     private Func1<ViewGroup, BindableViewHolder<?, T>> viewHolderFactory;
@@ -16,7 +17,8 @@ public class BindableAdapter<T> extends RecyclerView.Adapter<BindableViewHolder<
     public BindableAdapter(ObservableArrayList<T> items, Func1<ViewGroup, BindableViewHolder<?, T>> viewHolderFactory) {
         this.viewHolderFactory = viewHolderFactory;
         this.items = items;
-        items.addOnListChangedCallback(new WeakOnListChangedCallback<>(new ObservableList.OnListChangedCallback<ObservableList<T>>() {
+        //saved as field to avoid gargabe collector
+        onListChangedCallback = new ObservableList.OnListChangedCallback<ObservableList<T>>() {
             @Override public void onChanged(ObservableList<T> sender) {
                 notifyDataSetChanged();
             }
@@ -36,7 +38,8 @@ public class BindableAdapter<T> extends RecyclerView.Adapter<BindableViewHolder<
             @Override public void onItemRangeRemoved(ObservableList<T> sender, int positionStart, int itemCount) {
                 notifyItemRangeRemoved(positionStart, itemCount);
             }
-        }));
+        };
+        items.addOnListChangedCallback(new WeakOnListChangedCallback<>(onListChangedCallback));
     }
 
     @Override public BindableViewHolder<?, T> onCreateViewHolder(ViewGroup viewGroup, int i) {
