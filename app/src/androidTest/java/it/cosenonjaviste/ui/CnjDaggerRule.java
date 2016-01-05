02113a21ec12a50e0877d561193cd4e09fc2b8cd
Fabio Collini <fabio.collini@gmail.com>
2016-01-05 10:57:21 +0100

    DaggerMock 0.3

--- app/src/androidTest/java/it/cosenonjaviste/ui/CnjDaggerRule.java
+++ app/src/androidTest/java/it/cosenonjaviste/ui/CnjDaggerRule.java
@@ -4,12 +4,15 @@ import android.support.test.InstrumentationRegistry;
 
 import it.cosenonjaviste.androidtest.base.EspressoSchedulerManager;
 import it.cosenonjaviste.daggermock.DaggerMockRule;
+import it.cosenonjaviste.model.TwitterService;
+import it.cosenonjaviste.model.WordPressService;
 import it.cosenonjaviste.mv2m.rx.SchedulerManager;
 
 public class CnjDaggerRule extends DaggerMockRule<ApplicationComponent> {
     public CnjDaggerRule() {
         super(ApplicationComponent.class, new AppModule(getApp()));
-        override(SchedulerManager.class, new EspressoSchedulerManager());
+        provides(SchedulerManager.class, new EspressoSchedulerManager());
+        providesMock(WordPressService.class, TwitterService.class);
         set(component -> getApp().setComponent(component));
     }
 
