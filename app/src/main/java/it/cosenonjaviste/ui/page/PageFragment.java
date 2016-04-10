0e822c326079aec42b12cd2eabb525c1333a6faf
fabioCollini <fabio.collini@gmail.com>
2016-04-10 22:33:53 +0200

    Fixed css

--- app/src/main/java/it/cosenonjaviste/ui/page/PageFragment.java
+++ app/src/main/java/it/cosenonjaviste/ui/page/PageFragment.java
@@ -56,7 +56,7 @@ public class PageFragment extends ViewModelFragment<PageViewModel> {
             }
 
             @Override public WebResourceResponse shouldInterceptRequest(WebView view11, String url) {
-                if (url.contains("cosenonjaviste.it/wp-content/themes/flexform/style.css")) {
+                if (url.contains("codingjam.it/wp-content/themes/bliss/style.css?ver=4.4.2")) {
                     return getCssWebResourceResponseFromAsset();
                 }
                 if (url.startsWith("https://pbs.twimg.com/")
